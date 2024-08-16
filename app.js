5718de4c87add0c2f9aaa555f189d0251841d891
Edward <edward@egnition.io>
2024-08-16 13:33:04 +0600

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ console.log(tg.initDataUnsafe)
 
 // Display Telegram user's first name
 document.body.insertAdjacentHTML('beforeend', `<p>Hello, ${tg.initDataUnsafe.user.first_name}!</p>`);
+document.body.insertAdjacentHTML('beforeend', `<p>Platform, ${tg.platform}!</p>`);
+document.body.insertAdjacentHTML('beforeend', `<p>Version, ${tg.version()}!</p>`);
 
 // Send data to the bot when the button is clicked
 document.getElementById('send-data').addEventListener('click', () => {
